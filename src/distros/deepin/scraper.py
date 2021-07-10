d3df3f5c694964027ff655384d99131a934c516e
flameshikari <mail@hexed.pw>
2021-07-11 03:29:30 +0500

    new scraper: Deepin

---
+++ src/distros/deepin/scraper.py
@@ -0,0 +1,27 @@
+from public import *  # noqa
+
+
+base_urls = [
+  "https://mirror.yandex.ru/mirrors/deepin/releases/"
+]
+
+
+def init():
+    array = []
+    for base_url in base_urls:
+        html = bs(requests.get(base_url).text, "html.parser")
+        for version in html.find_all("a"):
+            version = version.get("href")
+            if version[0].isdigit():
+                html = bs(requests.get(base_url + version)
+                                  .text, "html.parser")
+                for filename in html.find_all("a"):
+                    filename = filename.get("href")
+                    if filename.endswith(".iso"):
+                        iso_url = base_url + version + filename
+                        iso_arch = get_iso_arch(iso_url)
+                        iso_size = get_iso_size(iso_url)
+                        iso_version = version[:-1]
+                        array.append((iso_url, iso_arch,
+                                      iso_size, iso_version))
+    return array
