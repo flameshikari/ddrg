5baff13f3cd3e335bb2a8d89f57424fdee63c88c
flameshikari <mail@hexed.pw>
2021-07-11 19:55:56 +0500

    fix scraper: BackBox

--- src/distros/backbox/scraper.py
+++ src/distros/backbox/scraper.py
@@ -1,21 +1,18 @@
 from public import *  # noqa
 
 
-base_urls = [
-    "https://mirror.truenetwork.ru/backbox/"
-]
+base_url = "https://mirror.truenetwork.ru/backbox/"
 
 
 def init():
     array = []
-    for base_url in base_urls:
-        html = bs(requests.get(base_url).text, "html.parser")
-        for filename in html.find_all("a"):
-            filename = filename.get("href")
-            if filename.endswith(".iso"):
-                iso_url = base_url + filename
-                iso_arch = get_iso_arch(iso_url)
-                iso_size = get_iso_size(iso_url)
-                iso_version = re.search(r"backbox-(\d+)", iso_url).group(1)
-                array.append((iso_url, iso_arch, iso_size, iso_version))
+    html = bs(requests.get(base_url).text, "html.parser")
+    for filename in html.find_all("a"):
+        filename = filename.get("href")
+        if filename.endswith(".iso"):
+            iso_url = base_url + filename
+            iso_arch = get_iso_arch(iso_url)
+            iso_size = get_iso_size(iso_url)
+            iso_version = re.search(r"backbox-(\d+)", iso_url).group(1)
+            array.append((iso_url, iso_arch, iso_size, iso_version))
     return array
