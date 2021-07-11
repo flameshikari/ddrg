381d1d7b4c3d845043dd620e430632ea34497650
flameshikari <mail@hexed.pw>
2021-07-11 16:18:07 +0500

    fix scraper: Absolute Linux

--- src/distros/absolute/scraper.py
+++ src/distros/absolute/scraper.py
@@ -1,21 +1,21 @@
 from public import *  # noqa
 
 
-base_urls = [
-    "https://slackware.uk/absolute/"
-]
+base_url = "https://slackware.uk/absolute/"
 
 
 def init():
     array = []
-    for base_url in base_urls:
-        html = bs(requests.get(base_url).text, "html.parser")
-        for filename in html.find_all("a"):
-            filename = filename.get("href")
-            if filename.endswith(".iso") and "live-current" not in filename:
-                iso_url = base_url + filename
-                iso_arch = get_iso_arch(iso_url)
-                iso_size = get_iso_size(iso_url)
+    html = bs(requests.get(base_url).text, "html.parser")
+    for filename in html.find_all("a"):
+        filename = filename.get("href")
+        if filename.endswith(".iso"):
+            iso_url = base_url + filename
+            iso_arch = get_iso_arch(iso_url)
+            iso_size = get_iso_size(iso_url)
+            try:
                 iso_version = re.search(r"-(\d{8})", iso_url).group(1)
-                array.append((iso_url, iso_arch, iso_size, iso_version))
+            except:
+                iso_version = "current"
+            array.append((iso_url, iso_arch, iso_size, iso_version))
     return array
