411f88653e2f3e3ca408a7e2314ca3ed61172e67
flameshikari <mail@hexed.pw>
2021-07-11 19:40:01 +0500

    fix scraper: elementary OS

--- src/distros/elementaryos/scraper.py
+++ src/distros/elementaryos/scraper.py
@@ -5,12 +5,10 @@ base_url = "https://elementary.io/"
 
 
 def init():
-    array = []
     html = str(bs(requests.get(base_url).text, "html.parser"))
     iso_url = "https:" + re.search(r'href="(//.*elementary.io/.*.iso)">',
                                    html).group(1)
     iso_arch = "x86_64"
     iso_size = get_iso_size(iso_url)
     iso_version = re.search(r"-(\d+(.\d+(.\d+)?)?)", iso_url).group(1)
-    array.append((iso_url, iso_arch, iso_size, iso_version))
-    return array
+    return [(iso_url, iso_arch, iso_size, iso_version)]
