a19954a19ae1a429dd9c98560950ed0bdba9bc91
flameshikari <mail@hexed.pw>
2021-07-11 19:55:25 +0500

    fix scraper: ArchLabs Linux

--- src/distros/archlabs/scraper.py
+++ src/distros/archlabs/scraper.py
@@ -1,22 +1,19 @@
 from public import *  # noqa
 
 
-base_urls = [
-    "https://sourceforge.net/projects/"
-    "archlabs-linux-minimo/rss?path=/ArchLabsMinimo/"
-]
+base_url = "https://sourceforge.net/projects/archlabs-linux-minimo/"
+           "rss?path=/ArchLabsMinimo/"
 
 
 def init():
     array = []
-    for base_url in base_urls:
-        xml = bs(requests.get(base_url).text, "xml")
-        values = xml.find_all("item")
-        for i in values:
-            content = i.find("content")
-            iso_url = content["url"][:-9]
-            iso_arch = get_iso_arch(iso_url)
-            iso_size = int(content["filesize"])
-            iso_version = re.search(r"-(\d{4}.\d{2}.\d{2})", iso_url).group(1)
-            array.append((iso_url, iso_arch, iso_size, iso_version))
+    xml = bs(requests.get(base_url).text, "xml")
+    values = xml.find_all("item")
+    for i in values:
+        content = i.find("content")
+        iso_url = content["url"][:-9]
+        iso_arch = get_iso_arch(iso_url)
+        iso_size = int(content["filesize"])
+        iso_version = re.search(r"-(\d{4}.\d{2}.\d{2})", iso_url).group(1)
+        array.append((iso_url, iso_arch, iso_size, iso_version))
     return array
