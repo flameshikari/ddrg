1778c4951e64a532d9d120d17fe8ae45466ba67d
flameshikari <mail@hexed.pw>
2021-07-11 19:43:57 +0500

    fix scraper: DragonFlyBSD

--- src/distros/dragonflybsd/scraper.py
+++ src/distros/dragonflybsd/scraper.py
@@ -1,21 +1,18 @@
 from public import *  # noqa
 
 
-base_urls = [
-    "https://mirror.tochlab.net/pub/DragonFlyBSD/iso-images/"
-]
+base_url = "https://mirror.tochlab.net/pub/DragonFlyBSD/iso-images/"
 
 
 def init():
     array = []
-    for base_url in base_urls:
-        html = bs(requests.get(base_url).text, "html.parser")
-        for filename in html.find_all("a"):
-            filename = filename.get("href")
-            if filename.endswith(".iso"):
-                iso_url = base_url + filename
-                iso_arch = get_iso_arch(iso_url)
-                iso_size = get_iso_size(iso_url)
-                iso_version = re.search(r"-(\d+.\d+.\d+)", iso_url).group(1)
-                array.append((iso_url, iso_arch, iso_size, iso_version))
+    html = bs(requests.get(base_url).text, "html.parser")
+    for filename in html.find_all("a"):
+        filename = filename.get("href")
+        if filename.endswith(".iso"):
+            iso_url = base_url + filename
+            iso_arch = get_iso_arch(iso_url)
+            iso_size = get_iso_size(iso_url)
+            iso_version = re.search(r"-(\d+.\d+.\d+)", iso_url).group(1)
+            array.append((iso_url, iso_arch, iso_size, iso_version))
     return array
