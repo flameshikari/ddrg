28185effcc03f43ba95293b9ca4292c53a1ca0c4
flameshikari <mail@hexed.pw>
2021-07-11 17:45:56 +0500

    fix scraper: Forget Windows Use Linux

--- src/distros/fwul/scraper.py
+++ src/distros/fwul/scraper.py
@@ -1,17 +1,12 @@
 from public import *  # noqa
 
 
-base_urls = [
-    "https://androidfilehost.com/?fid=8889791610682936459"
-]
+base_url = "https://androidfilehost.com/?fid=8889791610682936459"
 
 
 def init():
-    array = []
-    for base_url in base_urls:
-        iso_url = get_afh_url(base_url)
-        iso_arch = get_iso_arch(iso_url)
-        iso_size = get_iso_size(iso_url)
-        iso_version = re.search(r"v(\d+.\d+)", iso_url).group(1)
-        array.append((iso_url, iso_arch, iso_size, iso_version))
-    return array
+    iso_url = get_afh_url(base_url)
+    iso_arch = get_iso_arch(iso_url)
+    iso_size = get_iso_size(iso_url)
+    iso_version = re.search(r"v(\d+.\d+)", iso_url).group(1)
+    return [(iso_url, iso_arch, iso_size, iso_version)]
