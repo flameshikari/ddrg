91e895901d05771b3d2ee672e355e102212e7393
flameshikari <mail@hexed.pw>
2021-07-22 03:35:35 +0500

    new scraper: MX Linux

---
+++ src/distros/mx/scraper.py
@@ -0,0 +1,21 @@
+from public import *  # noqa
+
+
+def init():
+
+    array = []
+    base_url = "https://mxlinux.org/download-links"
+
+    html = bs(requests.get(base_url).text, "html.parser")
+    regex = re.compile("^.*\.iso\/download$")
+
+    for target in html.find_all("a", {"href": regex}):
+
+        iso_url = target["href"][:-9]
+        iso_arch = get_iso_arch(iso_url)
+        iso_size = get_iso_size(iso_url)
+        iso_version = re.search(r"-(\d+(\.\d+(\.\d+)?)?)", iso_url).group(1)
+
+        array.append((iso_url, iso_arch, iso_size, iso_version))
+
+    return array
