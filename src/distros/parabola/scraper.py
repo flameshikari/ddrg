60d6e3e2777ba3703cb50a461aa14c2eddd6a7e8
flameshikari <mail@hexed.pw>
2021-07-22 03:36:09 +0500

    new scraper: Parabola

---
+++ src/distros/parabola/scraper.py
@@ -0,0 +1,21 @@
+from public import *  # noqa
+
+
+def init():
+
+    array = []
+    base_url = "https://wiki.parabola.nu/Get_Parabola"
+
+    html = bs(requests.get(base_url).text, "html.parser")
+
+    for filename in html.find_all("a", {"href": re.compile("^.*\.iso$")}):
+
+        iso_url = filename["href"].replace("redirector.parabola.nu",
+                                           "mirror.yandex.ru/mirrors/parabola")
+        iso_arch = get_iso_arch(iso_url)
+        iso_size = get_iso_size(iso_url)
+        iso_version = re.search(r"-(\d+.\d+(.\d+)?)", iso_url).group(1)
+
+        array.append((iso_url, iso_arch, iso_size, iso_version))
+
+    return array
