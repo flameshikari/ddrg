e071bffa513cffafe87d9a6a5c99bee4d97c5ea2
flameshikari <mail@hexed.pw>
2021-07-31 02:02:25 +0500

    new scraper: AdminPE

---
+++ src/distros/adminpe/scraper.py
@@ -0,0 +1,17 @@
+from public import *  # noqa
+
+
+def init():
+
+    array = []
+    base_url = "https://getfile.dokpub.com/yandex/get"
+    iso_url = "https://disk.yandex.ru/d/YHflGF3zn3vf3w/AdminPE-4.4-UEFI.iso"
+    
+    iso_url = f"{base_url}/{iso_url}"
+    iso_version = "4.4"
+    iso_arch = "x86_64"
+    iso_size = get_iso_size(iso_url)
+    
+    array.append((iso_url, iso_arch, iso_size, iso_version))
+    
+    return array
