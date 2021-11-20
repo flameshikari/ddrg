ef6551963fe8a4a4127d24fbdc1a4603f6429836
flameshikari <mail@hexed.pw>
2021-11-20 19:13:44 +0500

    fix scraper: Manjaro Linux

--- src/distros/manjaro/scraper.py
+++ src/distros/manjaro/scraper.py
@@ -20,11 +20,13 @@ def init():
 
         for target in html.find_all("a", {"href": re.compile("^.*\.iso$")}):
 
-            iso_url = target["href"]
-            iso_arch = "amd64"
-            iso_size = get_iso_size(iso_url)
-            iso_version = re.search(r"/(\d+(.\d+(.\d+)?)?)/", iso_url).group(1)
-
-            array.append((iso_url, iso_arch, iso_size, iso_version))
+            try:
+                iso_url = target["href"]
+                iso_arch = "amd64"
+                iso_size = get_iso_size(iso_url)
+                iso_version = re.search(r"/(\d+(\.\w+(\.\w+)?)?)/", iso_url).group(1)
+                array.append((iso_url, iso_arch, iso_size, iso_version))
+            except:
+                pass
 
     return array
