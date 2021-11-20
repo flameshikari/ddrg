c81ec3aa79886f90166ac9a0984cdffb9b68d9c8
flameshikari <mail@hexed.pw>
2021-11-20 19:12:58 +0500

    fix scraper: ArcoLinux

--- src/distros/arcolinux/scraper.py
+++ src/distros/arcolinux/scraper.py
@@ -4,18 +4,23 @@ from public import *  # noqa
 def init():
 
     array = []
-    base_url = "https://bike.seedhost.eu/arcolinux/iso/"
 
+    base_url = "https://ant.seedhost.eu/arcolinux/iso/"
+    
     html = bs(requests.get(base_url).text, "html.parser")
+    version = html.find_all("tr")[-1].a.get_text()[1:]
+    
+    html = bs(requests.get(f"{base_url}v{version}/").text, "html.parser")
     regex = re.compile("^.*\.iso$")
 
     for target in html.find_all("a", {"class": "name", "href": regex}):
-
-        iso_url = f"{base_url}{target['href'][2:]}"
-        iso_arch = get_iso_arch(iso_url)
-        iso_size = get_iso_size(iso_url)
-        iso_version = re.search(r"v(\d+\.\d+\.\d+)", iso_url).group(1)
-
-        array.append((iso_url, iso_arch, iso_size, iso_version))
-
+        try:
+            iso_url = f"{base_url}v{version}/{target['href']}"
+            iso_arch = get_iso_arch(iso_url)
+            iso_size = get_iso_size(iso_url)
+            iso_version = re.search(r"-v(\d+(.\d+(.\d+)?)?)", iso_url).group(1)
+            array.append((iso_url, iso_arch, iso_size, iso_version))
+        except:
+            pass
+    
     return array
