2ad9bc1e73bf1515eef9d732d2ab4b293406c28c
flameshikari <mail@hexed.pw>
2021-11-20 19:12:00 +0500

    fix scraper: SparkyLinux

--- src/distros/sparky/scraper.py
+++ src/distros/sparky/scraper.py
@@ -15,11 +15,15 @@ def init():
 
         for target in html.find_all("a", {"href": re.compile("^.*\.iso/download$")}):
 
-            iso_url = target["href"][:-9]
-            iso_arch = get_iso_arch(iso_url)
-            iso_size = get_iso_size(iso_url)
-            iso_version = re.search(r"-(\d+\.\d+)", iso_url).group(1)
-
-            array.append((iso_url, iso_arch, iso_size, iso_version))
+            try:
+            
+                iso_url = target["href"][:-9]
+                iso_arch = get_iso_arch(iso_url)
+                iso_size = get_iso_size(iso_url)
+                iso_version = re.search(r"-(\d+\.\d+)", iso_url).group(1)
+            
+                array.append((iso_url, iso_arch, iso_size, iso_version))
+            
+            except: pass
 
     return array
