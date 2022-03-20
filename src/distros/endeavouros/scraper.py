75df41d87f3a032dd7b40c096ff5d15e0b771c91
flameshikari <mail@hexed.pw>
2022-03-20 06:46:26 +0500

    fix scraper: EndeavourOS

--- src/distros/endeavouros/scraper.py
+++ src/distros/endeavouros/scraper.py
@@ -7,14 +7,14 @@ def init():
     base_url = "https://endeavouros.com/latest-release"
 
     html = bs(requests.get(base_url).text, "html.parser")
-    filename = re.search(r'<strong>(endeavouros.*.iso)</strong>',
-                         str(html)).group(1)
+    
+    for filename in html.find_all("a", {"href": re.compile("^.*github.com.*\.iso$")}):
 
-    iso_url = f"https://github.com/endeavouros-team/ISO/releases/download/1-EndeavourOS-ISO-releases-archive/{filename}"
-    iso_arch = get_iso_arch(iso_url)
-    iso_size = get_iso_size(iso_url)
-    iso_version = re.search(r"-(\d{4}.\d{2}.\d{2})", iso_url).group(1)
+        iso_url = filename['href']
+        iso_arch = "x86_64"
+        iso_size = get_iso_size(iso_url)
+        iso_version = re.search(r"-(\d+(_\d+)?)", iso_url).group(1).replace('_', '.')
 
-    array.append((iso_url, iso_arch, iso_size, iso_version))
+        array.append((iso_url, iso_arch, iso_size, iso_version))
 
     return array
