a9e06132dc7941d0119728352d978d59e71f3212
flameshikari <mail@hexed.pw>
2022-03-20 05:54:54 +0500

    fix scraper: VyOS

--- src/distros/vyos/scraper.py
+++ src/distros/vyos/scraper.py
@@ -10,19 +10,14 @@ def init():
         "https://vyos.net/get/snapshots"
     ]
 
-    html = bs(requests.get(base_urls[0]).text, "html.parser")
-    for x in [0, 1]:
-        iso_urls.append(html.find_all("ul")[x].find_all("a")[0]["href"])
+    for base_url in base_urls:
 
-    html = bs(requests.get(base_urls[1]).text, "html.parser")
-    for x in html.find_all("a", {"href": re.compile("^.*\.iso$")}):
-        iso_urls.append(x["href"])
+        html = bs(requests.get(base_url).text, "html.parser")
 
-    for iso_url in iso_urls:
-
-        iso_arch = "amd64"
+        iso_url = html.find_all("ul")[0].find_all("a")[0]["href"]
+        iso_arch = get_iso_arch(iso_url)
         iso_size = get_iso_size(iso_url)
-        iso_version = re.search(r"vyos-(\d+.\d+(.\d+(-rc\d+)?)?)",
+        iso_version = re.search(r"vyos-(\d+.\d+(.\d+)?)",
                                 iso_url).group(1)
 
         array.append((iso_url, iso_arch, iso_size, iso_version))
