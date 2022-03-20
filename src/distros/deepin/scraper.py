2857535f27b168c638abd356d3b553fc65808ed1
flameshikari <mail@hexed.pw>
2022-03-20 06:46:14 +0500

    fix scraper: Deepin

--- src/distros/deepin/scraper.py
+++ src/distros/deepin/scraper.py
@@ -8,11 +8,18 @@ def init():
 
     html = bs(requests.get(base_url).text, "html.parser")
 
-    iso_version = html.find_all("a", {"href": re.compile("^.*/$")})[-1]["href"][:-1]
-    iso_url = f"{base_url}/{iso_version}/deepin-desktop-community-{iso_version}-amd64.iso"
-    iso_arch = get_iso_arch(iso_url)
-    iso_size = get_iso_size(iso_url)
+    tmp = html.find_all("a", {"href": re.compile("^.*/$")})
+    versions = [tmp[-1]["href"][:-1], tmp[-2]["href"][:-1]]
 
-    array.append((iso_url, iso_arch, iso_size, iso_version))
+    for iso_version in versions:
+
+        html = bs(requests.get(f"{base_url}/{iso_version}").text, "html.parser")
+        for filename in html.find_all("a", {"href": re.compile("^.*\.iso$")}):
+
+            iso_url = f"{base_url}/{iso_version}/{filename['href']}"
+            iso_arch = get_iso_arch(iso_url)
+            iso_size = get_iso_size(iso_url)
+
+            array.append((iso_url, iso_arch, iso_size, iso_version))
 
     return array
