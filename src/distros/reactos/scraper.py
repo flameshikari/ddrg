d63b165b9df9a5efeb553c4559bcfb67a31a24d8
flameshikari <mail@hexed.pw>
2022-03-20 05:54:40 +0500

    fix scraper: ReactOS

--- src/distros/reactos/scraper.py
+++ src/distros/reactos/scraper.py
@@ -7,18 +7,16 @@ def init():
     version_url = "https://reactos.org/download"
     base_url = "https://getfile.dokpub.com/yandex/get"
     iso_urls = [
-        "https://disk.yandex.ru/d/YHflGF3zn3vf3w/ReactOS-{}.iso",
-        "https://disk.yandex.ru/d/YHflGF3zn3vf3w/ReactOS-{}-Live.iso"
+        "https://disk.yandex.ru/d/YHflGF3zn3vf3w/ReactOS_Boot-{}.iso",
+        "https://disk.yandex.ru/d/YHflGF3zn3vf3w/ReactOS_Live-{}.iso"
     ]
 
-    html = bs(requests.get(version_url).text, "html.parser")
-    iso_version = re.search(r'React<span.*">OS</span> (\d+.\d+.\d+)</h1>',
-                            str(html)).group(1)
+    iso_version = "0.4.14"
+    iso_arch = "x86_64"
 
     for iso_url in iso_urls:
     
         iso_url = f"{base_url}/{iso_url.format(iso_version)}"
-        iso_arch = "x86_64"
         iso_size = get_iso_size(iso_url)
     
         array.append((iso_url, iso_arch, iso_size, iso_version))
