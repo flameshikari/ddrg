a6684ca831c02c5e4fb1da66f964c6340939ac7a
flameshikari <mail@hexed.pw>
2022-06-19 19:32:54 +0500

    fix sourceforge.net parser and scrapers

--- src/distros/kaos/scraper.py
+++ src/distros/kaos/scraper.py
@@ -9,8 +9,9 @@ def init():
 
     for iso_url in get.urls(url_base):
 
+        iso_size = iso_url['size']
+        iso_url = iso_url['url']
         iso_arch = get.arch(iso_url)
-        iso_size = get.size(iso_url)
         iso_version = re.search(regexp_version, iso_url).group(1)
         values.append((iso_url, iso_arch, iso_size, iso_version))
 
