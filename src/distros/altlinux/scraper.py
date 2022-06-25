0c3f45e7d3a1ece76f2ca8352841574d1644b0b7
flameshikari <mail@hexed.pw>
2022-06-25 19:39:11 +0500

    new scraper: altlinux

---
+++ src/distros/altlinux/scraper.py
@@ -0,0 +1,27 @@
+from main import *  # noqa
+
+
+def init():
+
+    values = []
+    regexp_version = re.compile(r'-(\d+(\.\d+)?)-')
+    url_bases = [
+        'https://getalt.org/en/alt-workstation/',
+        'https://getalt.org/en/alt-kworkstation/',
+        'https://getalt.org/en/alt-server/',
+        'https://getalt.org/en/alt-server-v/',
+        'https://getalt.org/en/alt-education/',
+        'https://getalt.org/en/simply/',
+        'https://getalt.org/en/alt-workstation/'
+    ]
+
+    for url_base in url_bases:
+        for iso_url in get.urls(url_base):
+
+            iso_arch = get.arch(iso_url)
+            iso_size = get.size(iso_url)
+            iso_version = re.search(regexp_version, iso_url).group(1)
+        
+            values.append((iso_url, iso_arch, iso_size, iso_version))
+
+    return values
