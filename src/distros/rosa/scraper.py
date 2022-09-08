7d213de9effb42cd6d25902d783c23b92fc96cc6
flameshikari <mail@hexed.pw>
2022-09-08 05:36:45 +0500

    new scraper: ROSA

---
+++ src/distros/rosa/scraper.py
@@ -0,0 +1,19 @@
+from main import *  # noqa
+
+
+def init():
+
+    values = []
+
+    regexp_version = re.compile(r'^.*\/iso\/\w+\.\w+\.R?(\d+(\.\d+)?)')
+    url_base = 'https://www.rosalinux.ru/rosa-linux-download-links/'
+
+    for iso_url in get.urls(url_base):
+
+        iso_arch = get.arch(iso_url)
+        iso_size = get.size(iso_url)
+        iso_version = re.search(regexp_version, iso_url).group(1)
+        
+        values.append((iso_url, iso_arch, iso_size, iso_version))
+
+    return values
