45f3cbd87ad579fade752359aaed0801b393d804
flameshikari <mail@hexed.pw>
2023-01-09 13:06:13 +0500

    fix scraper: nitrux

--- src/distros/nitrux/scraper.py
+++ src/distros/nitrux/scraper.py
@@ -4,14 +4,19 @@ from main import *  # noqa
 def init():
 
     values = []
-    regexp_version = re.compile(r'-(\d+)-')
 
-    iso_url = rq.get('https://nxos.org/download/standard/',
-                     allow_redirects=False).headers['Location']
-    iso_arch = get.arch(iso_url)
-    iso_size = get.size(iso_url)
-    iso_version = re.search(regexp_version, iso_url).group(1)
-    
-    values.append((iso_url, iso_arch, iso_size, iso_version))
+    url_base = 'https://sourceforge.net/projects/nitruxos/files/Release/ISO/'
+    url_version = 'https://nxos.org/'
+
+    regexp_version = re.compile(r'Nitrux (\d+\.\d+\.\d+)')
+    iso_version = re.search(regexp_version,
+                            str(rq.get(url_version).text)).group(1)
+
+    for iso_url in get.urls(url_base):
+
+        iso_size = iso_url['size']
+        iso_url = iso_url['url']
+        iso_arch = get.arch(iso_url)
+        values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
