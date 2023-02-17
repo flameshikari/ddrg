3b4b626a1ef8bfa6226ab5852f9edb21a51a0413
flameshikari <mail@hexed.pw>
2023-02-17 11:22:59 +0500

    update scraper: Slax

--- src/distros/slax/scraper.py
+++ src/distros/slax/scraper.py
@@ -4,15 +4,21 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['/packages']
+
+    url_bases = [
+        'https://ftp.linux.cz/pub/linux/slax/Slax-11.x/',
+        'https://ftp.linux.cz/pub/linux/slax/Slax-15.x/'
+    ]
+
     regexp_version = re.compile(r'-(\d+(\.\d+(\.\d+)?)?)')
-    url_base = 'https://www.slax.org/'
 
-    for iso_url in get.urls(url_base, exclude=exceptions):
+    for url_base in url_bases:
 
-        iso_arch = get.arch(iso_url)
-        iso_size = get.size(iso_url)
-        iso_version = re.search(regexp_version, iso_url).group(1)
-        values.append((iso_url, iso_arch, iso_size, iso_version))
+        for iso_url in get.urls(url_base):
+
+            iso_arch = get.arch(iso_url)
+            iso_size = get.size(iso_url)
+            iso_version = re.search(regexp_version, iso_url).group(1)
+            values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
