1ceac055a1c5638abd51be5acf857758aeb1be5d
flameshikari <mail@hexed.pw>
2023-05-24 16:29:50 +0500

    upgrade scraper: devuan

--- src/distros/devuan/scraper.py
+++ src/distros/devuan/scraper.py
@@ -4,15 +4,29 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['cd', 'CD'] + [f"_{str(i)}." for i in range(1, 4)]
+    exceptions = ['https://www.devuan.org', 'cd', 'CD'] + [f"_{str(i)}." for i in range(1, 4)]
     regexp_version = re.compile(r'_(\d+(\.\d+(\.\d+)?)?)')
-    url_base = 'https://mirror.math.princeton.edu/pub/devuan/'
+    regexp_arm_version = re.compile(r'Installer/(\w+)/')
+    url_bases = [
+        'https://files.devuan.org/',
+        'https://arm-files.devuan.org/Devuan-Arm64-Installer/'
+    ]
 
-    for iso_url in get.urls(url_base, exclude=exceptions,
-                                      recurse=True):
+    for iso_url in get.urls(url_bases[0], exclude=exceptions,
+                                          recurse=True):
         iso_arch = get.arch(iso_url)
         iso_size = get.size(iso_url)
         iso_version = re.search(regexp_version, iso_url).group(1)
         values.append((iso_url, iso_arch, iso_size, iso_version))
+
+    for iso_url in get.urls(url_bases[1], exclude=exceptions,
+                                          recurse=True):
+        iso_arch = get.arch(iso_url)
+        iso_size = get.size(iso_url)
+        iso_version = re.search(regexp_arm_version, iso_url).group(1)
+        values.append((iso_url, iso_arch, iso_size, iso_version))
+
+
+
 
     return values
