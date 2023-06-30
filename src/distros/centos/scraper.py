210c15efd1bb699050744b72006541a104a76522
flameshikari <mail@hexed.pw>
2023-07-01 03:22:52 +0500

    update scraper: CentOS

--- src/distros/centos/scraper.py
+++ src/distros/centos/scraper.py
@@ -4,13 +4,12 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['boot.iso', 'latest']
-    regexp_version = re.compile(r'-(\d+(.\d+(.\d+)?)?)')
+    exceptions = ['latest', 'source', '/os/']
+    regexp_version = re.compile(r'-(\d+(-\d+)?)')
     url_bases = [
-        'http://mirror.truenetwork.ru/centos/8-stream/isos/',
-        'http://mirror.truenetwork.ru/centos-stream/9-stream/BaseOS/',
-        'http://mirror.truenetwork.ru/centos/7.9.2009/isos/',
-        'http://mirror.truenetwork.ru/centos-altarch/7.9.2009/isos/'
+        'https://mirror.truenetwork.ru/centos-altarch/7/isos/',
+        'https://mirror.truenetwork.ru/centos/8-stream/isos/',
+        'https://mirror.truenetwork.ru/centos-stream/9-stream/BaseOS/'
     ]
 
     for url_base in url_bases:
@@ -18,7 +17,7 @@ def init():
                                           recurse=True):
             iso_arch = get.arch(iso_url)
             iso_size = get.size(iso_url)
-            iso_version = re.search(regexp_version, iso_url).group(1)
+            iso_version = re.search(regexp_version, iso_url).group(1).replace('-', ' ')
             values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
