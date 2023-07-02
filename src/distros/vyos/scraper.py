ffb951abec25cde26e5e5db29a46f5ace8c6b48c
flameshikari <mail@hexed.pw>
2023-07-02 07:54:47 +0500

    update scraper: VyOS

--- src/distros/vyos/scraper.py
+++ src/distros/vyos/scraper.py
@@ -7,18 +7,21 @@ def init():
     exceptions = ['latest.iso']
     regexp_url = re.compile(r'.*\.iso')
     regexp_version = re.compile(r'vyos-(\d+\.\d+(\.\d+)?)')
+    regexp_version_extra = re.compile(r'([S0-9]+)-amd64')
+
     url_bases = [
         'https://vyos.net/get/nightly-builds/',
-        'https://vyos.net/get/snapshots/'
+        'https://vyos.net/get/'
     ]
 
     for url_base in url_bases:
         for iso_url in get.urls(url_base, exclude=exceptions,
-                                          pattern=regexp_url,
-                                          recurse=True):
-            iso_arch = get.arch(iso_url)
+                                          pattern=regexp_url):
             iso_size = get.size(iso_url)
-            iso_version = re.search(regexp_version, iso_url).group(1)
+            #if iso_size is None: continue
+            iso_arch = get.arch(iso_url)
+            iso_version = re.search(regexp_version, iso_url).group(1) + ' ' + \
+                          re.search(regexp_version_extra, iso_url).group(1)
             values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
