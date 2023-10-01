ef8cb86f4d19823c983eb6f52171f35c00684381
flameshikari <mail@hexed.pw>
2023-10-01 12:16:45 +0500

    upgrade scraper: TrueNAS

--- src/distros/truenas/scraper.py
+++ src/distros/truenas/scraper.py
@@ -4,14 +4,18 @@ from main import *  # noqa
 def init():
 
     values = []
-    regexp_version = re.compile(r'-(\d+(.\d+)?-U\d+(\.\d+)?)')
-    url_base = 'https://download.freenas.org/latest/x64/'
+    regexp_version = re.compile(r'-(\d+\.\d+(-\d+\.\d+)?)')
+    url_bases = [
+        'https://www.truenas.com/download-truenas-core/',
+        'https://www.truenas.com/download-truenas-scale/'
+    ]
 
-    for iso_url in get.urls(url_base):
-        iso_arch = 'amd64'
-        iso_size = get.size(iso_url)
-        iso_version = re.search(regexp_version, iso_url).group(1)
-        
-        values.append((iso_url, iso_arch, iso_size, iso_version))
+    for url_base in url_bases:
+        for iso_url in get.urls(url_base):
+            iso_arch = 'amd64'
+            iso_size = get.size(iso_url)
+            iso_version = re.search(regexp_version, iso_url).group(1)
+            
+            values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
