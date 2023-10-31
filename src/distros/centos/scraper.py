89d79d745b2426c39b73601e7ae03bf7d91e30e7
flameshikari <mail@hexed.pw>
2023-10-31 23:53:15 +0500

    upgrade scraper: CentOS

--- src/distros/centos/scraper.py
+++ src/distros/centos/scraper.py
@@ -4,8 +4,8 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['latest', 'source', '/os/']
-    regexp_version = re.compile(r'-(\d+(-\d+)?)')
+    exceptions = ['source', '/os/']
+    regexp_version = re.compile(r'-(\d+(-\d+(\.\d+)?)?)')
     url_bases = [
         'https://mirror.truenetwork.ru/centos-altarch/7/isos/',
         'https://mirror.truenetwork.ru/centos/8-stream/isos/',
@@ -15,6 +15,7 @@ def init():
     for url_base in url_bases:
         for iso_url in get.urls(url_base, exclude=exceptions,
                                           recurse=True):
+            if re.search(r'-(20\d+(\.\d+)?)-', iso_url): continue
             iso_arch = get.arch(iso_url)
             iso_size = get.size(iso_url)
             iso_version = re.search(regexp_version, iso_url).group(1).replace('-', ' ')
