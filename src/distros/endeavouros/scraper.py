767268bcddc62c4ebed62cc605552526de48e9ed
flameshikari <mail@hexed.pw>
2024-02-26 01:01:57 +0500

    update scraper: EndeavourOS

--- src/distros/endeavouros/scraper.py
+++ src/distros/endeavouros/scraper.py
@@ -1,14 +1,16 @@
 from main import *  # noqa
+from datetime import datetime
 
 
 def init():
 
     values = []
-    regexp_version = re.compile(r'[-_](\d+[-_]\d+(_R\d+)?)')
-    url_base = 'https://mirror.moson.org/endeavouros/iso/'
+    regexp_version = re.compile(r'-(\d+\.\d+\.\d+)')
+    url_base = 'http://md.mirrors.hacktegic.com/endeavouros/iso/'
+    year = str(datetime.now().year)
 
     for iso_url in get.urls(url_base):
-
+        if not year in iso_url: continue
         iso_arch = "x86_64"
         iso_size = get.size(iso_url)
         iso_version = re.search(regexp_version, iso_url).group(1).replace('-', '_')
