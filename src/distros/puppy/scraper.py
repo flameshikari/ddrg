6ec062ccd58a264221a4fb2db271ae65b997d7b9
flameshikari <mail@hexed.pw>
2024-07-24 13:46:05 +0500

    upgrade scraper: Puppy Linux

--- src/distros/puppy/scraper.py
+++ src/distros/puppy/scraper.py
@@ -4,15 +4,27 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['distro.ibiblio.org']
-    regexp_version = re.compile(r'-(\d+\.\d+(\.\d+)?)')
-    url_base = 'https://puppylinux-woof-ce.github.io/'
+    regexp_version = re.compile(r'-(\d+\.\d+(-\d+)?)')
+    distros = [
+        'BionicPup32',
+        'BookwormPup32',
+        'BookwormPup64',
+        'FocalPup32',
+        'JammyPup32',
+        'NoblePup32',
+        'VoidPup32',
+        'VoidPup64',
+        'S15Pup32',
+        'S15Pup64'
+    ]
 
-    for iso_url in get.urls(url_base, exclude=exceptions):
-
-        iso_arch = get.arch(iso_url)
-        iso_size = get.size(iso_url)
-        iso_version = re.search(regexp_version, iso_url).group(1)
-        values.append((iso_url, iso_arch, iso_size, iso_version))
+    for distro in distros:
+        url_base = f'https://sourceforge.net/projects/pb-gh-releases/files/{distro}_release/'
+        for iso_url in get.urls(url_base):
+            iso_size = iso_url['size']
+            iso_url = iso_url['url']
+            iso_arch = get.arch(iso_url)
+            iso_version = re.search(regexp_version, iso_url).group(1)
+            values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
