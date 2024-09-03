63084e460ae17b34a4bb38845027dda8e1a369c6
flameshikari <mail@hexed.pw>
2024-09-03 21:15:33 +0500

    upgrade scraper: VyOS

--- src/distros/vyos/scraper.py
+++ src/distros/vyos/scraper.py
@@ -4,24 +4,19 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['latest.iso']
     regexp_url = re.compile(r'.*\.iso')
-    regexp_version = re.compile(r'vyos-(\d+\.\d+(\.\d+)?)')
-    regexp_version_extra = re.compile(r'([S0-9]+)-amd64')
+    regexp_version = re.compile(r'vyos-(.*)-rolling-(\d+)')
 
     url_bases = [
         'https://vyos.net/get/nightly-builds/',
-        'https://vyos.net/get/'
     ]
 
     for url_base in url_bases:
-        for iso_url in get.urls(url_base, exclude=exceptions,
-                                          pattern=regexp_url):
+        for iso_url in get.urls(url_base, pattern=regexp_url):
             iso_size = get.size(iso_url)
-            if iso_size is None: continue
             iso_arch = get.arch(iso_url)
             iso_version = re.search(regexp_version, iso_url).group(1) + ' ' + \
-                          re.search(regexp_version_extra, iso_url).group(1)
+                          re.search(regexp_version, iso_url).group(2)
             values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
