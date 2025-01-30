dd4a7d39709a57b3470abc8c7ed23ceb8b9ee7b4
flameshikari <mail@hexed.pw>
2025-01-31 01:39:48 +0500

    upgrade scraper: PeppermintOS

--- src/distros/peppermintos/scraper.py
+++ src/distros/peppermintos/scraper.py
@@ -4,17 +4,17 @@ from main import *  # noqa
 def init():
 
     values = []
-    exceptions = ['osdn.net']
     regexp_version = re.compile(r'Release Notes for Peppermint (\d+)')
-    url_base = 'https://peppermintos.com/guide/downloading/'
+    url_base = 'https://sourceforge.net/projects/peppermintos/files/isos/'
     url_version = 'https://peppermintos.com/release-notes/'
     response = rq.get(url_version)
     iso_version = re.search(regexp_version, str(response.text)).group(1)
 
-    for iso_url in get.urls(url_base, exclude=exceptions):
+    for iso_url in get.urls(url_base):
 
+        iso_size = iso_url['size']
+        iso_url = iso_url['url']
         iso_arch = get.arch(iso_url)
-        iso_size = get.size(iso_url)
         values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
