68df5ac148c80876c88c4ee153315966a67b92bf
flameshikari <mail@hexed.pw>
2025-01-31 01:23:28 +0500

    upgrade scraper: EndeavourOS

--- src/distros/endeavouros/scraper.py
+++ src/distros/endeavouros/scraper.py
@@ -5,15 +5,13 @@ from datetime import datetime
 def init():
 
     values = []
-    regexp_version = re.compile(r'-(\d+\.\d+\.\d+)')
+    regexp_version = re.compile(r'[-|_](\d+\.\d+\.\d+|\d+_\d+|\d+-\d+)')
     url_base = 'http://md.mirrors.hacktegic.com/endeavouros/iso/'
-    year = str(datetime.now().year)
 
     for iso_url in get.urls(url_base):
-        if not year in iso_url: continue
         iso_arch = "x86_64"
         iso_size = get.size(iso_url)
-        iso_version = re.search(regexp_version, iso_url).group(1).replace('-', '_')
+        iso_version = re.search(regexp_version, iso_url).group(1).replace('-', '.').replace('_', '.')
         values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
