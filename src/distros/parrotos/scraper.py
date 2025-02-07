14d0a4f69b503d91b7fa580e16e406b4ed6b836c
flameshikari <mail@hexed.pw>
2025-02-07 16:23:00 +0500

    update scraper: ParrotOS

--- src/distros/parrotos/scraper.py
+++ src/distros/parrotos/scraper.py
@@ -5,10 +5,14 @@ def init():
 
     values = []
     regexp_version = re.compile(r'-(\d+(.\d+(.\d+)?)?)')
-    url_base = 'https://mirror.truenetwork.ru/parrot/iso/current/'
+    url_base = 'https://deb.parrot.sh/direct/parrot/iso/'
+    exclude = [
+        'zorin', 'caine',
+        '/4.', '/5.',
+        'current', 'testing'
+    ]
 
-    for iso_url in get.urls(url_base):
-
+    for iso_url in get.urls(url_base, recurse=True, exclude=exclude):
         iso_arch = get.arch(iso_url)
         iso_size = get.size(iso_url)
         iso_version = re.search(regexp_version, iso_url).group(1)
