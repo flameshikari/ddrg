11f91d05b04e413a82a27358ecead78aa0b1ac7f
flameshikari <mail@hexed.pw>
2025-02-07 16:22:55 +0500

    update scraper: Kaspersky

--- src/distros/kaspersky/scraper.py
+++ src/distros/kaspersky/scraper.py
@@ -4,14 +4,19 @@ from main import *  # noqa
 def init():
 
     values = []
-    regexp_version = re.compile(r'(\d+\.\d+\.\d+\.\d+)')
-    url_base = 'https://support.kaspersky.ru/utility'
-    iso_url = get.urls(url_base)[0]
-    url_version = 'https://support.kaspersky.ru/krd' + re.search(r'\/20(\d+)\/', iso_url).group(1) + '/new/'
-    iso_version = re.search(regexp_version, str(rq.get(url_version).text)).group()
-    iso_arch = get.arch(iso_url)
-    iso_size = get.size(iso_url)
-        
-    values.append((iso_url, iso_arch, iso_size, iso_version))
+    regexp_version = re.compile(r'/(\d+)/krd.iso')
+
+    iso_urls = [
+        'https://rescuedisk.s.kaspersky-labs.com/updatable/2018/krd.iso',
+        'https://rescuedisk.s.kaspersky-labs.com/updatable/2024/krd.iso'
+    ]
+
+    iso_arch = 'x86_64'
+
+    for iso_url in iso_urls:
+        iso_size = get.size(iso_url)
+        iso_version = re.search(regexp_version, iso_url).group(1)
+
+        values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
