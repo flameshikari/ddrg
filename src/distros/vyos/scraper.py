13851d7464051f2eea5c52f3d4407a4ce62f5221
flameshikari <mail@hexed.pw>
2025-04-29 14:10:54 +0500

    update distro: VyOS

--- src/distros/vyos/scraper.py
+++ src/distros/vyos/scraper.py
@@ -5,7 +5,7 @@ def init():
 
     values = []
     regexp_url = re.compile(r'.*\.iso')
-    regexp_version = re.compile(r'vyos-(.*)-rolling-(\d+)')
+    regexp_version = re.compile(r'download/(.*)/vyos')
 
     url_bases = [
         'https://vyos.net/get/nightly-builds/',
@@ -15,8 +15,7 @@ def init():
         for iso_url in get.urls(url_base, pattern=regexp_url):
             iso_size = get.size(iso_url)
             iso_arch = get.arch(iso_url)
-            iso_version = re.search(regexp_version, iso_url).group(1) + ' ' + \
-                          re.search(regexp_version, iso_url).group(2)
+            iso_version = re.search(regexp_version, iso_url).group(1)
             values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
