d0f0b5de867b28575852ab3c73c5702930aaa80d
flameshikari <mail@hexed.pw>
2025-05-14 17:58:16 +0500

    update scraper: ArcoLinux

--- src/distros/arcolinux/scraper.py
+++ src/distros/arcolinux/scraper.py
@@ -4,15 +4,15 @@ from main import *  # noqa
 def init():
 
     values = []
-    regexp_version = re.compile('<a href=\'./v(.*?)\'')
-    url_base = 'https://ant.seedhost.eu/arcolinux/iso/'
-    response = rq.get(url_base)
-    iso_version = re.findall(regexp_version, str(response.text))[-1]
+    regexp_version = re.compile(r'(\d+\.\d+\.\d+)')
+    url_base = 'https://sourceforge.net/projects/arconetpro/files/'
 
-    for iso_url in get.urls(f'{url_base}v{iso_version}/'):
+    for iso_url in get.urls(url_base):
 
+        iso_size = iso_url['size']
+        iso_url = iso_url['url']
         iso_arch = get.arch(iso_url)
-        iso_size = get.size(iso_url)
+        iso_version = re.search(regexp_version, iso_url).group(1)
         values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
