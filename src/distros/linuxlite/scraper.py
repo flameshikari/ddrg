692dc7a3c37f9bbca9561d09a2898fb1edf5daf4
flameshikari <mail@hexed.pw>
2025-06-15 23:11:18 +0500

    update scraper: Linux Lite

--- src/distros/linuxlite/scraper.py
+++ src/distros/linuxlite/scraper.py
@@ -4,16 +4,15 @@ from main import *  # noqa
 def init():
 
     values = []
-    regexp_version = re.compile(r'Linux Lite (\d+(\.\d+)?)')
-    url_version = 'https://www.linuxliteos.com/download.php'
-    response = rq.get(url_version)
-    iso_version = re.search(regexp_version, str(response.text)).group(1)
-    url_base = f'http://repo.linuxliteos.com/linuxlite/isos/{iso_version}/'
+    regexp_version = re.compile(r'-(\d+\.\d+)?-')
+    url_base = 'https://sourceforge.net/projects/linux-lite/files/'
 
     for iso_url in get.urls(url_base):
 
+        iso_size = iso_url['size']
+        iso_url = iso_url['url']
         iso_arch = get.arch(iso_url)
-        iso_size = get.size(iso_url)
+        iso_version = re.search(regexp_version, iso_url).group(1)
         values.append((iso_url, iso_arch, iso_size, iso_version))
 
     return values
