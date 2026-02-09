abf05acf2b354222d5104712c0fca4a501cf4cbc
flameshikari <mail@hexed.pw>
2026-02-09 15:31:18 +0500

    update scraper: OpenSUSE

--- src/distros/opensuse/scraper.py
+++ src/distros/opensuse/scraper.py
@@ -1,46 +1,3 @@
-# from helpers import *
-
-# info = {
-#     'name': 'OpenSUSE',
-#     'url': 'https://opensuse.org'
-# }
-
-# def init():
-
-#     url_version = 'https://get.opensuse.org/'
-#     response = str(rq.get(url_version).text)
-
-#     version_leap = re.search(re.compile(r'Leap (\d+(\.\d+)?)'), response).group(1)
-#     version_leap_micro = re.search(re.compile(r'Leap Micro (\d+(\.\d+)?)'), response).group(1)
-
-#     url_bases = [
-#         'https://get.opensuse.org/tumbleweed',
-#         f'https://get.opensuse.org/leapmicro/{version_leap_micro}/',
-#         f'https://get.opensuse.org/leap/{version_leap}/'
-#     ]
-
-#     values = []
-#     regexp_version = re.compile(r'/(\d+(\.\d+)?)/')
-
-#     for url_base in url_bases:
-#         for iso_url in get.urls(url_base):
-
-#             iso_arch = get.arch(iso_url)
-#             iso_size = get.size(iso_url)
-
-#             try:
-#                 version = re.search(regexp_version, iso_url).group(1)
-#                 if '/leap-micro/' in iso_url:
-#                     iso_version = 'Leap Micro ' + version
-#                 elif '/leap/' in iso_url:
-#                     iso_version = 'Leap ' + version
-#             except:
-#                 iso_version = 'Tumbleweed'
-
-#             values.append((iso_url, iso_arch, iso_size, iso_version))
-
-#     return values
-
 from shared import *
 
 info = ns(
@@ -55,10 +12,10 @@ def init():
     regexp = r'\/(\d+\.\d+)\/'
 
     target = [
-        'https://mirror.tspu.ru/opensuse/slowroll/',
-        'https://mirror.tspu.ru/opensuse/tumbleweed/',
-        'https://mirror.tspu.ru/opensuse/distribution/leap/',
-        'https://mirror.tspu.ru/opensuse/distribution/leap-micro/',
+        'https://ftp.sh.cvut.cz/opensuse/slowroll/',
+        'https://ftp.sh.cvut.cz/opensuse/tumbleweed/',
+        'https://ftp.sh.cvut.cz/opensuse/distribution/leap/',
+        'https://ftp.sh.cvut.cz/opensuse/distribution/leap-micro/',
     ]
     
     exclude = [
