77cd37637e7b548d8a7daf11695ef4a4a9dba14a
flameshikari <mail@hexed.pw>
2026-02-09 15:31:23 +0500

    update scraper: PeppermintOS

--- src/distros/peppermintos/scraper.py
+++ src/distros/peppermintos/scraper.py
@@ -5,11 +5,12 @@ info = ns(
     url='https://peppermintos.com',
 )
 
-def get_version():
-    target = 'https://peppermintos.com/release-notes/'
-    regexp = re.compile(r'Release Notes for Peppermint (\d+)')
-    response = str(rq.get(target).text)
-    return re.search(regexp, response).group(1)
+# def get_version():
+#     target = 'https://sourceforge.net/p/peppermintos/pepwiki/BuildDate/'
+#     regexp = re.compile(r'Build Date: (\d+-\d+-\d+) ')
+#     response = str(rq.get(target).text)
+#     print(response)
+#     return re.search(regexp, response).group(1)
 
 @scraper
 def init():
@@ -17,7 +18,7 @@ def init():
 
     target = 'https://sourceforge.net/projects/peppermintos/files/isos/'
     
-    version = get_version()
+    # version = get_version()
 
     for url, size in get.urls(target):
 
@@ -27,7 +28,7 @@ def init():
             arch=arch,
             size=size,
             url=url,
-            version=version
+            version='',
         ))
 
     return values
